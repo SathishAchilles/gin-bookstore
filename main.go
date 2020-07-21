b9387a0f93e3fb44cba5da2f4bc0071fd3fee8a5
sathish <reachtosathish.b@gmail.com>
2020-07-21 10:32:47 +0530

    route all books request

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
     "net/http"
     "github.com/gin-gonic/gin"
     "github.com/sathishachilles/gin-bookstore/models"
+    "github.com/sathishachilles/gin-bookstore/controllers"
 )
 
 func main() {
     r := gin.Default()
     models.ConnectDatabase()
+    r.GET("/books", controllers.FindBooks)
     r.Run()
 }
