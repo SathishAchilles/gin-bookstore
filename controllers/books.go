d83d33df6c05fcd08a0d58cfc951911174935a95
sathish <reachtosathish.b@gmail.com>
2020-07-23 00:48:25 +0530

    create books
    enables creation of books

--- controllers/books.go
+++ controllers/books.go
@@ -15,3 +15,23 @@ func FindBooks(c *gin.Context) {
 
     c.JSON(http.StatusOK, gin.H{"data": books})
 }
+
+// Create Book
+func CreateBook(c *gin.Context) {
+    var input CreateBookInput
+    if err := c.ShouldBindJSON(&input); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+   book := models.Book{Title: input.Title, Author: input.Author}
+   models.DB.Create(&book)
+   c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+// input form schemes
+
+type CreateBookInput struct {
+    Title string `json:"title" binding:"required"`
+    Author string `json:"author" binding:"required"`
+}
--- main.go
+++ main.go
@@ -10,5 +10,6 @@ func main() {
     r := gin.Default()
     models.ConnectDatabase()
     r.GET("/books", controllers.FindBooks)
+    r.POST("/books", controllers.CreateBook)
     r.Run()
 }
