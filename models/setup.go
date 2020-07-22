754643da0f4fcd5d3e4633e7777aca14bd60828d
sathish <reachtosathish.b@gmail.com>
2020-07-23 00:26:43 +0530

    rename book & setup, fix typo
    filenames had a nasty space at the end of the file causing modules to crash while importing

--- models/setup.go
+++ models/setup.go
@@ -8,7 +8,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-    database, err := gorm.open("sqlite3", "test.db")
+    database, err := gorm.Open("sqlite3", "test.db")
 
     if err != nil {
         panic("failed to connect database")
